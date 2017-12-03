bce262b10037db1a11ee89ecf9e7b5ff98062e1e
Jason C.H <ctrysbita@outlook.com>
2017-12-03 13:48:35 +0800

    fix ID page ui

--- lib/idpage.dart
+++ lib/idpage.dart
@@ -54,75 +54,80 @@ class _IDPageState extends State<IDPage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("Me"),),
-      body: new Container(
-        margin: const EdgeInsets.only(right: 5.0),
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new TextField(
-              controller: _usernameController,
-              decoration: new InputDecoration(
-                hintText: 'Campus ID',
-                icon: new Icon(
-                  Icons.account_circle,
+      body: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 20.0),
+            child: new Column(
+              children: <Widget>[
+                new TextField(
+                  controller: _usernameController,
+                  decoration: new InputDecoration(
+                    hintText: 'Campus ID',
+                    icon: new Icon(
+                      Icons.account_circle,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            new TextField(
-              controller: _idPasswordController,
-              obscureText: true,
-              decoration: new InputDecoration(
-                hintText: 'Campus ID Password',
-                icon: new Icon(
-                  Icons.lock_outline,
+                new TextField(
+                  controller: _idPasswordController,
+                  obscureText: true,
+                  decoration: new InputDecoration(
+                    hintText: 'Campus ID Password',
+                    icon: new Icon(
+                      Icons.lock_outline,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            new TextField(
-              controller: _epaymentPasswordController,
-              obscureText: true,
-              decoration: new InputDecoration(
-                hintText: 'EPayment Password',
-                icon: new Icon(
-                  Icons.lock_outline,
+                new TextField(
+                  controller: _epaymentPasswordController,
+                  obscureText: true,
+                  decoration: new InputDecoration(
+                    hintText: 'EPayment Password',
+                    icon: new Icon(
+                      Icons.lock_outline,
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-            new FlatButton(
-              child: new Container(
-                height: 30.0,
-                decoration: new BoxDecoration(
-                  color: Theme
-                      .of(context)
-                      .accentColor,
-                ),
-                child: new Center(
-                    child: new Text("Sign In",
-                        style: new TextStyle(
-                          color: Colors.white,
-                        ))),
+          ),
+          new FlatButton(
+            child: new Container(
+              height: 30.0,
+              decoration: new BoxDecoration(
+                color: Theme
+                    .of(context)
+                    .accentColor,
               ),
-              onPressed: () {
-                _authCampusID().then(_authEPayment).then((bool auth){
-                  if (auth){
-                    var infoMap = {
-                      "id":_usernameController.text,
-                      "campus":_idPasswordController.text,
-                      "epayment":_epaymentPasswordController.text,
-                    };
-                    _saveLoginInfo(JSON.encode(infoMap));
-                    Scaffold.of(context).showSnackBar(
-                        new SnackBar(content: new Text("Success!")));
-                  }
-                  else
-                    Scaffold.of(context).showSnackBar(
-                        new SnackBar(content: new Text("Failed!")));
-                });
-              },
+              child: new Center(
+                  child: new Text("Sign In",
+                      style: new TextStyle(
+                        color: Colors.white,
+                      ))),
             ),
-          ],
-        ),
+            onPressed: () {
+              _authCampusID().then(_authEPayment).then((bool auth) {
+                if (auth) {
+                  var infoMap = {
+                    "id": _usernameController.text,
+                    "campus": _idPasswordController.text,
+                    "epayment": _epaymentPasswordController.text,
+                  };
+                  _saveLoginInfo(JSON.encode(infoMap));
+                  Scaffold.of(context).showSnackBar(
+                      new SnackBar(content: new Text("Success!")));
+                }
+                else
+                  Scaffold.of(context).showSnackBar(
+                      new SnackBar(content: new Text("Failed!")));
+              });
+            },
+          ),
+        ],
       ),
+
     );
   }
 }
